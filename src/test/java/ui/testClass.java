7ac8005de76539b446ce57e661657c6140920b31
minskole7 <minskole7@gmail.com>
2024-02-18 19:48:20 +0530

    added

--- src/test/java/ui/testClass.java
+++ src/test/java/ui/testClass.java
@@ -1,6 +1,6 @@
 package ui;
-import static org.junit.Assert.assertArrayEquals;
-
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -8,10 +8,29 @@ import common.BaseTest;
 
 public class testClass extends BaseTest{
 	
-	@Test
+	@Test(enabled = false)
 	public static void TestOne() {
 		driver.get("https://www.google.com");
 		Assert.assertEquals("chinmay","ram") ;
+	}
+	
+	@Test
+	public static void FileUpload() {
+		driver.get("https://webdriveruniversity.com/File-Upload/index.html?filename=Unit.pdf");
+		String file_path = "C:\\Users\\LENOVO\\eclipse-workspace\\seleniumpom\\src\\test\\java\\files\\Unit.pdf";
+		WebElement  fileInput = driver.findElement(By.id("myFile"));	
+		fileInput.sendKeys(file_path);
+		driver.findElement(By.id("submit-button")).click();
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		String txt = driver.switchTo().alert().getText();
+		driver.switchTo().alert().accept();
+		Assert.assertEquals("Your file has now been uploaded!", txt);
+		
 	}
 	
 
